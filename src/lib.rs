36f84c0f8c5043821ba8887b07eebc91e44b98d4
Matthias Wolf <matthias@sushinara.net>
2020-03-05 06:57:16 +0100

    Apply formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 extern crate num_complex;
 extern crate num_traits;
 
-use self::num_traits::Float;
 use self::num_traits::cast;
+use self::num_traits::Float;
 use num_complex::Complex;
 use std::iter;
 // use std::ops::{Add, Div, Mul, Sub};
@@ -14,21 +14,26 @@ struct Binning<T> {
 }
 
 impl<T> Binning<T>
-    where T: Float
-    // Add<Output=T> + Div<Output=T> + Mul<Output=T> + Sub<Output=T> + From<u32> + From<f32>
+where
+    T: Float, // Add<Output=T> + Div<Output=T> + Mul<Output=T> + Sub<Output=T> + From<u32> + From<f32>
 {
     fn bin(&self, n: T) -> Option<u16> {
         match cast((n - self.min) * self.scale) {
-            Some(c) => if c >= self.num { None } else { Some(c) },
-            _ => None
+            Some(c) => {
+                if c >= self.num {
+                    None
+                } else {
+                    Some(c)
+                }
+            }
+            _ => None,
         }
     }
 
     fn iter(&self) -> impl Iterator<Item = T> + '_ {
-        (0..self.num).map(move |n|
-                          self.min + (cast::<u16, T>(n).unwrap() +
-                                      cast::<f32, T>(0.5).unwrap()) /
-                          self.scale)
+        (0..self.num).map(move |n| {
+            self.min + (cast::<u16, T>(n).unwrap() + cast::<f32, T>(0.5).unwrap()) / self.scale
+        })
     }
 }
 
@@ -39,12 +44,13 @@ pub struct Histogram<'a, T> {
 }
 
 impl<'a, T> Histogram<'a, T>
-    where T: Float
+where
+    T: Float,
 {
     pub fn centers(&self) -> impl Iterator<Item = (T, T)> + '_ {
-        self.yaxis.iter()
-            .flat_map(move |y|
-                      self.xaxis.iter().zip(iter::repeat(y)))
+        self.yaxis
+            .iter()
+            .flat_map(move |y| self.xaxis.iter().zip(iter::repeat(y)))
     }
 
     pub fn values(&self, layer: usize) -> impl Iterator<Item = &u32> {
@@ -55,20 +61,31 @@ impl<'a, T> Histogram<'a, T>
         let nx = self.xaxis.bin(x);
         let ny = self.yaxis.bin(y);
         if !nx.is_some() || !ny.is_some() {
-            return
+            return;
         }
         let idx = nx.unwrap() as usize + ny.unwrap() as usize * self.xaxis.num as usize;
         self.bins[i][idx] += 1;
     }
 
-    pub fn new(xmin: T, xmax: T, xnum: u16,
-               ymin: T, ymax: T, ynum: u16,
-               bins: Vec<&'a mut [u32]>) -> Histogram<'a, T>
-    {
-        let xaxis = Binning { scale: T::from(xnum).unwrap() / (xmax - xmin),
-                              min: xmin, num: xnum };
-        let yaxis = Binning { scale: T::from(ynum).unwrap() / (ymax - ymin),
-                              min: ymin, num: ynum };
+    pub fn new(
+        xmin: T,
+        xmax: T,
+        xnum: u16,
+        ymin: T,
+        ymax: T,
+        ynum: u16,
+        bins: Vec<&'a mut [u32]>,
+    ) -> Histogram<'a, T> {
+        let xaxis = Binning {
+            scale: T::from(xnum).unwrap() / (xmax - xmin),
+            min: xmin,
+            num: xnum,
+        };
+        let yaxis = Binning {
+            scale: T::from(ynum).unwrap() / (ymax - ymin),
+            min: ymin,
+            num: ynum,
+        };
         Histogram { xaxis, yaxis, bins }
     }
 }
@@ -80,25 +97,34 @@ pub struct ComplexSequence<T> {
 }
 
 impl<T> Iterator for ComplexSequence<T>
-    where T: Float
+where
+    T: Float,
 {
     type Item = Complex<T>;
 
     fn next(&mut self) -> Option<Complex<T>> {
         self.z = self.z * self.z + self.c;
         if self.z.norm() > self.r {
-            return None
+            return None;
         }
         Some(self.z)
     }
 }
 
 pub fn mandelbrot<T>(c: Complex<T>) -> ComplexSequence<T>
-    where T: Float
+where
+    T: Float,
 {
     let start: T = T::from(0.0).unwrap();
     let radius: T = T::from(2.0).unwrap();
-    ComplexSequence { z: Complex { re: start, im: start }, c: c, r: radius }
+    ComplexSequence {
+        z: Complex {
+            re: start,
+            im: start,
+        },
+        c: c,
+        r: radius,
+    }
 }
 
 #[cfg(test)]
@@ -107,14 +133,22 @@ mod tests {
 
     #[test]
     fn binning_iter() {
-        let bins = Binning { scale: 2.0, min: 0.0, num: 2 };
+        let bins = Binning {
+            scale: 2.0,
+            min: 0.0,
+            num: 2,
+        };
         let res: Vec<f64> = bins.iter().collect();
         assert_eq!(res, vec![0.25, 0.75]);
     }
 
     #[test]
     fn binning_fill() {
-        let bins = Binning { scale: 1.0, min: 0.0, num: 2 };
+        let bins = Binning {
+            scale: 1.0,
+            min: 0.0,
+            num: 2,
+        };
         let b1 = bins.bin(0.1);
         assert_eq!(b1, Some(0));
         let b2 = bins.bin(5.0);
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
+extern crate bincode;
 extern crate clap;
 extern crate image;
 extern crate num_complex;
 extern crate palette;
 extern crate pbr;
-extern crate bincode;
 
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
 
+use bincode::{deserialize_from, serialize_into};
 use clap::{App, Arg};
 use num_complex::Complex;
-use palette::{Gradient, Hsv, LinSrgb, Srgb, Pixel};
+use palette::{Gradient, Hsv, LinSrgb, Pixel, Srgb};
 use pbr::ProgressBar;
 use std::fs::File;
 use std::io;
 use std::io::BufWriter;
-use bincode::{serialize_into, deserialize_from};
 
 mod lib;
 
 #[derive(Deserialize)]
 struct Layer {
     iterations: usize,
-    colors: Vec<[f32; 3]>
+    colors: Vec<[f32; 3]>,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -42,13 +42,13 @@ impl PartialEq<Layer> for LayerData {
 #[derive(Clone, Copy, Deserialize, Serialize, PartialEq)]
 struct Area {
     x: [f32; 2],
-    y: [f32; 2]
+    y: [f32; 2],
 }
 
 #[derive(Clone, Copy, Deserialize, Serialize, PartialEq)]
 struct Dimensions {
     x: u16,
-    y: u16
+    y: u16,
 }
 
 impl Dimensions {
@@ -74,9 +74,10 @@ struct Cache {
 
 impl PartialEq<Configuration> for Cache {
     fn eq(&self, other: &Configuration) -> bool {
-        if self.area != other.area ||
-           self.dimensions != other.dimensions ||
-           self.layers.len() != other.layers.len() {
+        if self.area != other.area
+            || self.dimensions != other.dimensions
+            || self.layers.len() != other.layers.len()
+        {
             return false;
         }
         for (&ref a, &ref b) in self.layers.iter().zip(other.layers.iter()) {
@@ -93,31 +94,40 @@ impl Cache {
         Cache {
             area: c.area,
             dimensions: c.dimensions,
-            layers: c.layers.iter().map(|l| {
-                LayerData {
+            layers: c
+                .layers
+                .iter()
+                .map(|l| LayerData {
                     iterations: l.iterations,
                     data: vec![0; c.dimensions.size()],
-                }
-            }).collect(),
-            valid: false
+                })
+                .collect(),
+            valid: false,
         }
     }
 
     fn load(filename: &str, config: &Configuration) -> Cache {
         let file = match File::open(filename) {
             Ok(f) => f,
-            _ => return Cache::new(config)
+            _ => return Cache::new(config),
         };
         match bincode::deserialize_from(file) {
             Ok(c) => {
-                if c == *config { c } else { Cache::new(config) }
-            },
-            _ => Cache::new(config)
+                if c == *config {
+                    c
+                } else {
+                    Cache::new(config)
+                }
+            }
+            _ => Cache::new(config),
         }
     }
 }
 
-fn color_cache<'a>(cache: &'a Cache, config: &'a Configuration) -> impl Iterator<Item = impl Iterator<Item = LinSrgb> + 'a> + 'a {
+fn color_cache<'a>(
+    cache: &'a Cache,
+    config: &'a Configuration,
+) -> impl Iterator<Item = impl Iterator<Item = LinSrgb> + 'a> + 'a {
     cache.layers.iter().zip(config.layers.iter()).map(|(d, c)| {
         let imax = match d.data.iter().max() {
             Some(&n) => n,
@@ -125,13 +135,15 @@ fn color_cache<'a>(cache: &'a Cache, config: &'a Configuration) -> impl Iterator
         };
 
         let gradient: Gradient<Hsv> = Gradient::new(
-            c.colors.iter()
-                    .map(|[r, g, b]|
-                         Hsv::from(Srgb::new(*r, *g, *b)))
+            c.colors
+                .iter()
+                .map(|[r, g, b]| Hsv::from(Srgb::new(*r, *g, *b))),
         );
 
         let colors: Vec<_> = gradient.take(imax as usize + 1).collect();
-        d.data.iter().map(move |&i| LinSrgb::from(colors[i as usize]))
+        d.data
+            .iter()
+            .map(move |&i| LinSrgb::from(colors[i as usize]))
     })
 }
 
@@ -141,20 +153,27 @@ fn populate_cache(cache: &mut Cache) {
         None => 0,
     };
 
-    let iterations: Vec<_> = cache.layers.iter().map(|l| l.iterations).enumerate().collect();
+    let iterations: Vec<_> = cache
+        .layers
+        .iter()
+        .map(|l| l.iterations)
+        .enumerate()
+        .collect();
 
     let mut data: Vec<&mut [u32]> = vec![];
     for l in cache.layers.iter_mut() {
         data.push(&mut l.data[..]);
     }
 
-    let mut histo = lib::Histogram::new(cache.area.x[0],
-                                        cache.area.x[1],
-                                        cache.dimensions.x,
-                                        cache.area.y[0],
-                                        cache.area.y[1],
-                                        cache.dimensions.y,
-                                        data);
+    let mut histo = lib::Histogram::new(
+        cache.area.x[0],
+        cache.area.x[1],
+        cache.dimensions.x,
+        cache.area.y[0],
+        cache.area.y[1],
+        cache.dimensions.y,
+        data,
+    );
 
     let mut bar = ProgressBar::new(cache.dimensions.size() as u64);
     bar.show_counter = false;
@@ -166,9 +185,7 @@ fn populate_cache(cache: &mut Cache) {
     let centers: Vec<_> = histo.centers().collect();
     for (x, y) in centers {
         let c = Complex { re: x, im: y };
-        let nums: Vec<_> = lib::mandelbrot(c)
-                               .take(max_iter)
-                               .collect();
+        let nums: Vec<_> = lib::mandelbrot(c).take(max_iter).collect();
         for (layer, maximum) in iterations.iter() {
             if nums.len() < *maximum {
                 for z in nums.iter() {
@@ -190,20 +207,26 @@ fn main() -> Result<(), io::Error> {
         .version("0.1.0")
         .author("Matthias Wolf <m@sushinara.net>")
         .about("Generate Buddhabrot images")
-        .arg(Arg::with_name("cache")
-                 .takes_value(true)
-                 .long("cache")
-                 .help("A cache file to use; default: 'cache.yaml'"))
-        .arg(Arg::with_name("config")
-                 .takes_value(true)
-                 .required(true)
-                 .index(1)
-                 .help("A yaml configuration file"))
-        .arg(Arg::with_name("filename")
-                 .takes_value(true)
-                 .required(true)
-                 .index(2)
-                 .help("The output filename"))
+        .arg(
+            Arg::with_name("cache")
+                .takes_value(true)
+                .long("cache")
+                .help("A cache file to use; default: 'cache.yaml'"),
+        )
+        .arg(
+            Arg::with_name("config")
+                .takes_value(true)
+                .required(true)
+                .index(1)
+                .help("A yaml configuration file"),
+        )
+        .arg(
+            Arg::with_name("filename")
+                .takes_value(true)
+                .required(true)
+                .index(2)
+                .help("The output filename"),
+        )
         .get_matches();
 
     let config_file = File::open(cli.value_of("config").unwrap()).unwrap();
@@ -218,7 +241,9 @@ fn main() -> Result<(), io::Error> {
         let mut f = BufWriter::new(File::create(cache_filename).unwrap());
         match serialize_into(&mut f, &cache) {
             Ok(r) => r,
-            _ => {println!("serialization error!");}
+            _ => {
+                println!("serialization error!");
+            }
         };
     }
 
@@ -231,18 +256,17 @@ fn main() -> Result<(), io::Error> {
     //         Some(x) => *x
     //     };
 
-
     // }
 
-    let temp: Vec<[u8; 3]> = data.iter()
-                                 .map(|c| c.into_format()
-                                           .into_raw())
-                                 .collect();
+    let temp: Vec<[u8; 3]> = data.iter().map(|c| c.into_format().into_raw()).collect();
     let buffer: &[u8] = &temp.concat();
-    image::save_buffer(cli.value_of("filename").unwrap(),
-                       buffer,
-                       config.dimensions.x as u32,
-                       config.dimensions.y as u32,
-                       image::RGB(8)).unwrap();
+    image::save_buffer(
+        cli.value_of("filename").unwrap(),
+        buffer,
+        config.dimensions.x as u32,
+        config.dimensions.y as u32,
+        image::RGB(8),
+    )
+    .unwrap();
     Ok(())
 }
